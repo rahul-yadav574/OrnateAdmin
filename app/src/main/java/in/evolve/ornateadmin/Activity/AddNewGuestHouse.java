d898a6f4570921dd0f40657acffe5052a156be32
rajeevyadav <rajeevy899@gmail.com>
2017-01-08 00:49:33 +0530

    Add pg add guest house landing layout added

--- app/src/main/java/in/evolve/ornateadmin/Activity/AddNewGuestHouse.java
+++ app/src/main/java/in/evolve/ornateadmin/Activity/AddNewGuestHouse.java
@@ -2,14 +2,36 @@ package in.evolve.ornateadmin.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import in.evolve.ornateadmin.R;
 
 public class AddNewGuestHouse extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_guest_house);
+
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_material);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.home)
+        {
+            AddNewGuestHouse.this.finish();
+        }
+        return true;
     }
 }
