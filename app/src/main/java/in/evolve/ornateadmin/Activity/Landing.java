bd6fc34d5c302ee4d24c073eeb26b748dd9b518d
rahulyadav20111995 <rahulyadav574@gmail.com>
2017-01-10 11:28:38 +0530

    Error in gradle dont knw

--- app/src/main/java/in/evolve/ornateadmin/Activity/Landing.java
+++ app/src/main/java/in/evolve/ornateadmin/Activity/Landing.java
@@ -1,15 +1,68 @@
 package in.evolve.ornateadmin.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import in.evolve.ornateadmin.R;
 
-public class Landing extends AppCompatActivity {
+public class Landing extends AppCompatActivity implements View.OnClickListener {
+
+    private Button addGuestHouse,addPg,ghBookChck,pgBookChk,pgVisitChk,lisProCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        addGuestHouse = (Button) findViewById(R.id.add_new_gh);
+        addPg = (Button) findViewById(R.id.add_new_pg);
+        ghBookChck = (Button) findViewById(R.id.gh_book_check);
+        pgBookChk = (Button) findViewById(R.id.pg_book_check);
+        pgVisitChk = (Button) findViewById(R.id.pg_visit_check);
+        lisProCheck = (Button) findViewById(R.id.list_house_check);
+
+        addGuestHouse.setOnClickListener(this);
+        addPg.setOnClickListener(this);
+        ghBookChck.setOnClickListener(this);
+        pgBookChk.setOnClickListener(this);
+        pgVisitChk.setOnClickListener(this);
+        lisProCheck.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.add_new_gh:
+                Intent intent = new Intent(Landing.this,AddNewPg.class);
+                startActivity(intent);
+                break;
+            case R.id.add_new_pg:
+                Intent intent1 = new Intent(Landing.this,AddNewGuestHouse.class);
+                startActivity(intent1);
+                break;
+            case R.id.gh_book_check:
+                Intent intent2 = new Intent(Landing.this,GuestHouseBookActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.pg_book_check:
+                Intent intent3 = new Intent(Landing.this,PgBookActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.pg_visit_check:
+                Intent intent4 = new Intent(Landing.this,PgVisitActivity.class);
+                startActivity(intent4);
+                break;
+            case R.id.list_house_check:
+                Intent intent5 = new Intent(Landing.this,PgListRequest.class);
+                startActivity(intent5);
+                break;
+        }
+
     }
 }
--- app/src/main/java/in/evolve/ornateadmin/Activity/LoginActivity.java
+++ app/src/main/java/in/evolve/ornateadmin/Activity/LoginActivity.java
@@ -1,7 +1,11 @@
 package in.evolve.ornateadmin.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import in.evolve.ornateadmin.R;
 
@@ -11,5 +15,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText email = (EditText) findViewById(R.id.login_email_input);
+        final EditText pass = (EditText) findViewById(R.id.login_pass_input);
+
+        Button button  = (Button) findViewById(R.id.login_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (email.getText().toString().equals("jagdeepkumar01@gmail.com") && pass.getText().toString().equals("9050290015#"))
+                {
+                    Intent intent  = new Intent(LoginActivity.this,Landing.class);
+                    startActivity(intent);
+                    LoginActivity.this.finish();
+                }
+
+            }
+        });
     }
 }
